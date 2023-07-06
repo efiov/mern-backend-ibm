e837edbc0a528e66e53ff182ca6c6617d0dca5f5
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-06 16:35:53 +0300

    update database

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
-mongoose.connect(`mongodb+srv://t5:omegat5@omega.iw7e8lr.mongodb.net/?retryWrites=true&w=majority`, {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });

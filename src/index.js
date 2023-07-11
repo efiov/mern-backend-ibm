6b47c1697552e9516fec615417acb9f3f4f2c263
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-11 13:08:31 +0300

    bodyparser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const database = require('./config/database');
-const eventRoutes = require('./routes/eventRoutes');
+const express = require("express");
+const database = require("./config/database");
+const eventRoutes = require("./routes/eventRoutes");
+const cors = require("cors");
 
 const app = express();
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 app.use(express.json());
-app.use('/events', eventRoutes);
+app.use(cors());
+//app.use('/events', eventRoutes);
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+app.use("/", userRoutes);
 
 const port = 3001;
 app.listen(port, () => {

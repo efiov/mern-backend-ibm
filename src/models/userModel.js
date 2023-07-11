131e0fa081c359a9dbd0ba0a74e0db8eb1b99b72
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-11 13:06:51 +0300

    update user schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,22 +1,29 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: { type: Boolean, default: false },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
-
+  { timestamps: true }
+);
 
 const User = mongoose.model("users", userSchema);
 
 module.exports = User;
-

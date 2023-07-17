7f55a845a7a5d8ed6e256ddd7ba596452674bcf1
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-17 13:23:35 +0300

    reqeventrou

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { events } = require("./models/eventModel");
 const bodyParser = require("body-parser");
+const eventRoutes = require('./routes/eventRoutes');
+const database = require('./config/database');
 const cors = require("cors");
 const port = 3001;
 const app = express();
@@ -14,7 +16,11 @@ app.use(
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
-app.use('/events', eventRoutes);
+app.use("/", eventRoutes);
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

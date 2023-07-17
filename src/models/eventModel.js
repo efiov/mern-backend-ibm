661a6809b37f9cffaf6b09bee7ef3039f522a27e
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-17 11:48:32 +0300

    eventModel location map schema

--- src/models/eventModel.js
+++ src/models/eventModel.js
@@ -1,24 +1,36 @@
-const mongoose = require('mongoose');
+const { Double } = require("mongodb");
+const mongoose = require("mongoose");
 
-const eventSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const eventSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    lat: {
+      type: Double,
+      require: true,
+    },
+    long: {
+      type: Double,
+      require: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  location: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
-const Event = mongoose.model('Event', eventSchema);
+const Event = mongoose.model("Event", eventSchema);
 
 module.exports = Event;

44acc725f806b820bb3c84ea748a00b9bf8be222
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-21 22:05:15 +0300

    create event with email

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,5 +1,6 @@
 const Event = require("../models/eventModel");
 const Group = require("../models/groupModel");
+const User = require("../models/userModel");
 const { sendEmail } = require("../Service/emailService");
 
 exports.getEvents = async (req, res) => {
@@ -14,15 +15,20 @@ exports.getEvents = async (req, res) => {
 exports.createEvent = async (req, res) => {
   const event = new Event({
     name: req.body.name,
-    description: req.body.description,
+    type: req.body.type,
     date: req.body.date,
     location: req.body.location,
   });
+  const selectedGroups = req.body.selectedGroups;
 
   try {
     const newEvent = await event.save();
-    res.status(201).json(newEvent);
-    const selectedGroups = req.body.selectedGroups;
+    const addGroups = await Event.findByIdAndUpdate(
+      newEvent._id,
+      { $push: { groups: { $each: selectedGroups } } },
+      { new: true, upsert: true }
+    );
+
     const groupEmails = await Group.find(
       { _id: { $in: selectedGroups } },
       "members"
@@ -33,15 +39,19 @@ exports.createEvent = async (req, res) => {
       []
     );
 
-    // Customize the subject and email content to your needs
+    await User.updateMany(
+      { _id: { $in: allEmails } },
+      { $push: { events: newEvent._id } },
+      { new: true, upsert: true }
+    );
+
+    const users = await User.find({ _id: { $in: allEmails } });
+    const emails = users.map((user) => user.email);
     const subject = "New Event Invitation";
     const text = `You are invited to the event: ${event.name}.`;
     const html = `<p>You are invited to the event: ${event.name}.</p>`;
-
-    // Send the email to all members of the selected groups
-    await sendEmail(allEmails, subject, text, html);
-
-    res.status(201).json(savedEvent);
+    await sendEmail(emails, subject, text, html);
+    res.status(201).json(addGroups);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Failed to create event." });

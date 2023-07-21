927832eb44653f7b4594d8ad34dc1babcc8a2580
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-21 11:33:32 +0300

    clean index

--- src/index.js
+++ src/index.js
@@ -1,38 +1,26 @@
-const express = require('express');
-const database = require('./config/database');
-const eventRoutes = require('./routes/eventRoutes');
-const { events } = require('./models/eventModel');
+const express = require("express");
+const database = require("./config/database");
 const bodyParser = require("body-parser");
-const passport = require("passport");
-const { users } = require("./models/userModel");
-const userRoutes = require('./routes/userRoutes');
-const groupRoutes = require('./routes/groupRoutes');
-const cors = require('cors');
-
-// ... Other middleware and configurations
-
- // Make sure the path is correct
-
-
+const userRoutes = require("./routes/userRoutes");
+const groupRoutes = require("./routes/groupRoutes");
+const eventRoutes = require("./routes/eventRoutes");
+const cors = require("cors");
+const port = 3001;
 const app = express();
 
 app.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: true,
   })
 );
 
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
-//app.use('/events', eventRoutes);
-app.use('/groups', groupRoutes);
+app.use("/", userRoutes);
+app.use("/", groupRoutes);
+app.use("/", eventRoutes);
 
-app.use(passport.initialize());
-require('./config/passport')(passport);
-app.use('/', userRoutes);
-
-const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });

74c3cf642f464d1db6b2e77ff359a742aa8787a7
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-21 15:13:31 +0300

    update userContl

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,7 @@ const User = require("../models/userModel");
 exports.getUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.json(events);
+    res.json(users);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }

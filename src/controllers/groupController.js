c7c3b328675091ba3827c53d8b24d1819c7cb8c6
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-21 14:12:49 +0300

    createGroup

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -1,24 +1,33 @@
 const Group = require("../models/groupModel");
+const User = require("../models/userModel");
 
 exports.getGroups = async (req, res) => {
   try {
     const groups = await Group.find();
     res.json(groups);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch groups' });
+    res.status(500).json({ error: "Failed to fetch groups" });
   }
 };
 
 exports.createGroup = async (req, res) => {
-    try {
-      const groupName = req.body.name;
-      const groupPeople = req.body.people;
-      const group = new Group({ name: groupName, people: groupPeople });
-      await group.save();
-      res.status(201).json(group);
-    } catch (err) {
-      console.error('Error while creating a group:', err);
-      res.status(500).json({ error: 'Failed to create group' });
-    }
-  };
-  
+  const groupName = req.body.name;
+  const membersIds = req.body.members;
+
+  try {
+    const group = await Group.create({
+      name: groupName,
+    });
+
+    const addMembers = await Group.findByIdAndUpdate(
+      group._id,
+      { $push: { members: { $each: membersIds } } },
+      { new: true, upsert: true }
+    );
+
+    res.status(201).json(addMembers);
+  } catch (err) {
+    console.error("Error while creating a group:", err);
+    res.status(500).json({ error: "Failed to create group" });
+  }
+};
--- src/routes/groupRoutes.js
+++ src/routes/groupRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const groupController = require("../controllers/groupController");
 
-router.get("/getGroups", groupController.getAllGroups);
+router.get("/getGroups", groupController.getGroups);
 router.post("/createGroup", groupController.createGroup);
 
 module.exports = router;

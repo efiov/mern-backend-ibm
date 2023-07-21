4f774d811b11fc2651c58b3cf972e6ecb7fe510a
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-21 21:45:07 +0300

    update nodenaiker host

--- src/Service/emailService.js
+++ src/Service/emailService.js
@@ -1,21 +1,19 @@
 const nodemailer = require("nodemailer");
 
-// Create a Nodemailer transporter using SMTP
 const transporter = nodemailer.createTransport({
-  host: "your_smtp_host",
-  port: 587, // Change to your SMTP port if different
-  secure: false, // Set to true if using SSL/TLS
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: "your_smtp_username",
-    pass: "your_smtp_password",
+    user: "viosdev@outlook.com",
+    pass: "Tra9ws2kg35pCxS8",
   },
 });
 
-// Function to send an email
 const sendEmail = async (to, subject, text, html) => {
   try {
     const info = await transporter.sendMail({
-      from: "event_Planer@example.com",
+      from: "event_planer@example.com",
       to: to,
       subject: subject,
       text: text,

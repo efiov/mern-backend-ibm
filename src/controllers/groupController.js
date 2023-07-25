51ab734087b00f2f8d11492ac633628e94cee48b
Viorel Stefan <114824366+efiov@users.noreply.github.com>
2023-07-25 11:13:19 +0300

    fix crategr

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -45,16 +45,3 @@ exports.deleteGroup = async (req, res) => {
     return res.status(400).json(err);
   }
 };
-
-exports.createGroup = async (req, res) => {
-  try {
-    const groupName = req.body.name;
-    const groupPeople = req.body.people;
-    const group = new Group({ name: groupName, people: groupPeople });
-    await group.save();
-    res.status(201).json(group);
-  } catch (err) {
-    console.error("Error while creating a group:", err);
-    res.status(500).json({ error: "Failed to create group" });
-  }
-};
